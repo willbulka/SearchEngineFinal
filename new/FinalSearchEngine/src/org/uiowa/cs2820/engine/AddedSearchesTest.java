14c6c3ee2a467ac575ac93ff6df4fcf97c4fa5eb
jkinney76 <joshua-kinney@uiowa.edu>
2014-12-02 18:09:14 -0600

    Create AddedSearchesTest.java

---
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/AddedSearchesTest.java
@@ -0,0 +1,28 @@
+package finalPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddedSearchesTest {
+	
+	//Testing of a prefixSearch() method still to be written.
+	
+	//Testing of a greaterThanSearch() method still to be written.
+	
+	//Testing of a lessThanSearch() method still to be written.
+	
+	//Testing of a containsSearch() method still to be written.
+	
+	//Testing of a dateRangeSearch() method still to be written.
+	
+	//testing of a searchByIdentifiers() method still to be written.
+	
+	//Another method to be added include timeSearch() or a generalSearch() method 
+	//for a generic object.
+	
+	//All methods are to be included in the AddedSearches class.
+	//In all roughly 7 tests are planned for code coverage. 
+	//This is by no means a definite number though.
+
+}
