683cb7df2bbc06b5ea51180cb0feb212ae22bc4c
Tim Miller <tim-miller@uiowa.edu>
2014-12-08 17:05:42 -0600

    Iterator Class - Edit #1
    Setup the layout for the Iterator class & test file.
    Still will need an IteratorInterface.java file, too.

---
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/IteratorClass.java
@@ -0,0 +1,35 @@
+/*
+ Tim Miller (Problem Five)
+*/
+package org.uiowa.cs2820.engine;
+
+import java.util.Iterator; 
+
+public class IteratorClass {
+
+	public static void hasNext(){
+		//TODO
+	}
+	
+	public static void next(){
+		//TODO
+	}
+	
+	public static void showAll(){
+		//TODO
+	}
+	
+	public static void remove(){
+		//TODO
+	}
+	
+	public static void add(){
+		//TODO
+	}
+	
+	public static void hasPrevious(){
+		//NOT SURE IF NEEDED
+	}
+	
+	
+}
---
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/IteratorTest.java
@@ -0,0 +1,54 @@
+/*
+ Tim Miller (Problem Five's Unit Test)
+ */
+
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IteratorTest {
+
+	@Before
+	public void test0() {
+		//IF NEEDED
+	}
+	
+	@Test
+	public void test1() {
+		//TODO
+		//Test for next() method.
+	}
+	
+	@Test
+	public void test2() {
+		//TODO
+		//Test for hasNext() method.
+	}
+	
+	@Test
+	public void test3() {
+		//TODO
+		//Test for add() method.
+	}
+	
+	@Test
+	public void test4() {
+		//TODO
+		//Test for remove() method.
+	}
+	
+	@Test
+	public void test5() {
+		//TODO
+		//Test for showAll() method.
+	}
+	
+	@Test
+	public void test6() {
+		//IF NEEDED
+		//Test for hasPrevious() method.
+	}	
+}
