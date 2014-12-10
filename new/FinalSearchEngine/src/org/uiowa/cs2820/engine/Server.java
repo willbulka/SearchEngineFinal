576a02ceae8c8f6c5eb2e5af3bcbaa6cf7bbb76b
chochua <chochua@iowa.uiowa.edu>
2014-12-10 14:43:54 -0600

    Trying to integrate the server

--- new/FinalSearchEngine/src/org/uiowa/cs2820/engine/Server.java
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/Server.java
@@ -1,54 +1,51 @@
 package org.uiowa.cs2820.engine;
 
-//package edu.iowa;
 
-import java.util.List;
-import java.util.ArrayList;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-// fixed import for javax.ws.rs 
-// by clicking the red cross mark beside
-// then clicking the fix project setup
-// applied to glassfish and codehaus (seperately) too
 
-// Alternative: 
-// use PA1, then import SearchEngineV2 to it
-
-import org.glassfish.jersey.internal.util.collection.MultivaluedStringMap;
-import org.codehaus.jettison.json.JSONArray;
-import org.codehaus.jettison.json.JSONTokener;
-import org.codehaus.jettison.json.JSONException;
-
-
-@Path("/")
-@Produces("text/html")
+/**
+ * Root resource (exposed at "server" path)
+ */
+@Path("Server")
 public class Server {
-	@GET		    	// @PUT  // Query will be GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("Catenate")	// "Catenate" - URL path (e.g. http:// ... ) 
-    @Consumes(MediaType.APPLICATION_JSON)	// from Jersey
-    public String Catenate(String req) {
-                List<String> params = new ArrayList<String>();
-                JSONTokener S = new JSONTokener(req);
-                JSONArray D = new JSONArray(); // empty by default
-                try { D = new JSONArray(S); }
-                catch (JSONException j) {};
-                for (int i=0; i<D.length(); i++) {
-                        String e = "";
-                        try { e = D.getString(i); }
-                        catch (JSONException j) {};
-                        params.add(e);
-                        // System.out.println("reponse position" + i + "equals" + e);
-                    }
-                D = new JSONArray();
-                for (int i=params.size()-1; i>=0; i--) D.put(params.get(i));
-                String eBody = D.toString();
-                return eBody;
-                
-    }
-}
 
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    /**
+     * Window Builder plug-in for GUI
+     * or
+     * Scanner for plain-text-shell-UI
+     * import java.util.Scanner;
+     * 
+     * public SearchBox() {
+     *		Scanner sc = new Scanner(System.in);
+     *		while (exit == false)  { // escape if exit is true
+     *  		System.out.println("Enter Search Term or Sentence");
+     *			String name = sc.nextLine();
+     *
+     *			// show result
+     *			// provide options
+     *			// exit command
+     * 
+     * }
+     * 
+     * public options() {
+     * }
+     * 
+     * 
+     */
+    
+}
