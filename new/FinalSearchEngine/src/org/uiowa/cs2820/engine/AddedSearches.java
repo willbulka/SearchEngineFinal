60b5951b44d367e82759dfc10923185d9ac4037b
jkinney76 <joshua-kinney@uiowa.edu>
2014-12-09 20:13:37 -0600

    Update AddedSearches.java

--- new/FinalSearchEngine/src/org/uiowa/cs2820/engine/AddedSearches.java
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/AddedSearches.java
@@ -6,18 +6,17 @@ import java.util.ArrayList;
 public class AddedSearches {
 	
 	//GreaterThanSearch implemented here
-	@SuppressWarnings("null")
-	public ArrayList<String> greaterThanSearch(Field f, ArrayList<Node> D){
-		ArrayList<String> s = null;
-		for(Node n : D){
-			Field dataField = n.getKey();
+	public static ArrayList<String> greaterThanSearch(Field f, ArrayList<Field> D){
+		ArrayList<String> s = new ArrayList<String>();
+		for(Field n : D){
+			//Field dataField = n.getKey();
 			String name0 = f.getFieldName();
-			String name1 = dataField.getFieldName();
+			String name1 = n.getFieldName();
 			if(name0.compareTo(name1) == 0){
 				String value0 = (String) f.getFieldValue();
-				String value1 = (String) dataField.getFieldValue();
+				String value1 = (String) n.getFieldValue();
 				if(value0.compareTo(value1) == 1){
-					s.add(dataField.toString());
+					s.add(n.toString());
 				}
 			}
 		}			
@@ -26,18 +25,17 @@ public class AddedSearches {
 	}
 	
 	//LessThanSearch implemented here
-	@SuppressWarnings("null")
-	public ArrayList<String> lessThanSearch(Field f, ArrayList<Node> D){
-		ArrayList<String> s = null;
-		for(Node n : D){
-			Field dataField = n.getKey();
+	public static ArrayList<String> lessThanSearch(Field f, ArrayList<Field> D){
+		ArrayList<String> s = new ArrayList<String>();
+		for(Field n : D){
+			//Field dataField = n.getKey();
 			String name0 = f.getFieldName();
-			String name1 = dataField.getFieldName();
+			String name1 = n.getFieldName();
 			if(name0.compareTo(name1) == 0){
 				String value0 = (String) f.getFieldValue();
-				String value1 = (String) dataField.getFieldValue();
+				String value1 = (String) n.getFieldValue();
 				if(value0.compareTo(value1) == -1){
-					s.add(dataField.toString());
+					s.add(n.toString());
 				}
 			}
 		}			
@@ -45,33 +43,31 @@ public class AddedSearches {
 		
 	}
 	
-	@SuppressWarnings("null")
-	public ArrayList<String> prefixSearch(Field f, ArrayList<Node> D){
-		ArrayList<String> s = null;
+	public static ArrayList<String> prefixSearch(Field f, ArrayList<Field> D){
+		ArrayList<String> s = new ArrayList<String>();
 
-		for(Node n : D){
+		for(Field n : D){
 			
 			boolean check = true;
 			
-			Field dataField = n.getKey();
+			//Field dataField = n.getKey();
 			String name0 = f.getFieldName();
-			String name1 = dataField.getFieldName();
+			String name1 = n.getFieldName();
 			
 			if(name0.compareTo(name1) == 0){
 				
 				String[] value0 = ((String) f.getFieldValue()).split("(?!^)");
-				String[] value1 = ((String) dataField.getFieldValue()).split("(?!^)");
+				String[] value1 = ((String) n.getFieldValue()).split("(?!^)");
 				
 				for(int i = 0; i < value0.length; i++){
 					
 					if(value0[i].compareTo(value1[i]) != 0){
 						check = false;
-						break;
 					}
 				}
 					
 				if(check){
-					s.add(dataField.toString());
+					s.add(n.toString());
 				}
 			
 			}
@@ -80,23 +76,22 @@ public class AddedSearches {
 		
 	}
 	
-	@SuppressWarnings("null")
-	public ArrayList<String> containsSearch(Field f, ArrayList<Node> D){
-		ArrayList<String> s = null;
+	public static ArrayList<String> containsSearch(Field f, ArrayList<Field> D){
+		ArrayList<String> s = new ArrayList<String>();
 
-		for(Node n : D){
+		for(Field n : D){
 			
-			Field dataField = n.getKey();
+			//Field dataField = n.getKey();
 			String name0 = f.getFieldName();
-			String name1 = dataField.getFieldName();
+			String name1 = n.getFieldName();
 			
 			if(name0.compareTo(name1) == 0){
 				
 				String value0 = (String) f.getFieldValue();
-				String value1 = (String) dataField.getFieldValue();
+				String value1 = (String) n.getFieldValue();
 				
 				if(value1.contains(value0)){
-					s.add(dataField.toString());
+					s.add(n.toString());
 				}
 			}
 		}			
