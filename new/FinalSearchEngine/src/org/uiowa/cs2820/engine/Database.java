3927e51348e6a7e0bacb55b0b03f8b28c29da4e5
jkinney76 <joshua-kinney@uiowa.edu>
2014-12-11 16:08:27 -0600

    Update Database.java

--- new/FinalSearchEngine/src/org/uiowa/cs2820/engine/Database.java
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/Database.java
@@ -4,4 +4,5 @@ public interface Database {
 	  public abstract String[] fetch(Field f);  // fetch a Node by Field 
 	  public abstract void store(Field f, String id); // store a Field/id
 	  public abstract void remove(String id); // delete document (by identifier) 
+	  public abstract ArrayList<Node> fetchAll();
 	  }
