494da45f77307f2096b98acf3c7da4d93aea416b
Twoody <tanner.l.woody@gmail.com>
2014-12-11 16:35:39 -0600

    Added some thoughts to hasNext; Can be applied to other methods

--- new/FinalSearchEngine/src/org/uiowa/cs2820/engine/IteratorClass.java
+++ new/FinalSearchEngine/src/org/uiowa/cs2820/engine/IteratorClass.java
@@ -7,8 +7,17 @@ import java.util.Iterator;
 
 public class IteratorClass {
 
-	public static void hasNext(){
-		//TODO
+	public static void hasNext(//Parans){
+		/*TODO:
+		    If this hasNext, we need to decide whether that means that:
+		        1. Going back to the top of the file returns false
+		        2. Going back to the value that is currently being held (i.e. 1st result, sent back to first)
+
+		    Should use: 
+		         for (type item: iterableCollection) {
+                          // do something to item
+                         }
+		 */  
 	}
 	
 	public static void next(){
